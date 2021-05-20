a5fd3c49886daa45169548d8aaaafaf1d5636dc2
relaernu <relaernu@gmail.com>
2021-05-20 23:57:57 +1000

    Change tensorflow-cpu

--- model_testing.py
+++ model_testing.py
@@ -2,7 +2,7 @@ import model1
 import model2
 from datetime import datetime
 # testing
-#create_model()
+model1.create_model()
 dt = datetime.strptime("20210520", "%Y%m%d").date()
 print(f"Model1 Predict Date {dt}: {model1.predict_date(dt)}")
 
